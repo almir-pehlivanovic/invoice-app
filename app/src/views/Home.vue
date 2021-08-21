fa0a8224cdfbd71b3e68f0b3e98ef3d5b3eb6a14
almir-pehlivanovic <pehlivanovicalmir1@gmail.com>
2021-08-21 15:40:56 +0200

    Add logo on Home page

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -2,18 +2,27 @@
   <div class="-mt-28">
     <div
       class="
+        -mt-16
+        -translate-x-1/2 -translate-y-1/2
         absolute
         font-bold
-        inset-y-1/2
-        left-0
+        left-1/2
         text-4xl text-center text-gray-800
-        w-full
+        top-1/2
+        transform
       "
     >
+      <img
+        class="mb-6 mx-auto"
+        width="69"
+        height="88"
+        src="@/assets/custom-logo.png"
+        alt="Custom logo"
+      />
       <h1 class="-mt-4 drop-shadow-md">invoice</h1>
     </div>
     <div class="absolute bottom-36 flex justify-center left-0 px-8 w-full">
-      <router-link class="w-full max-w-2xl" :to="{name: 'Invoices'}">
+      <router-link class="w-full max-w-2xl" :to="{ name: 'Invoices' }">
         <button
           class="
             bg-gray-800
