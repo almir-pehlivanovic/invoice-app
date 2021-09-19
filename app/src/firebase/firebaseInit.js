c6ac39d2a9d1cc189b71b3901a979e47bb153842
almir-pehlivanovic <pehlivanovicalmir1@gmail.com>
2021-09-19 14:42:59 +0200

    Firebase / Firestore Setup

---
+++ app/src/firebase/firebaseInit.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, doc, setDoc } from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyD-NQ83t4c9wyHx2mTRaVJWHTxxxGblOnA",
+    authDomain: "invoice-app-90dbd.firebaseapp.com",
+    projectId: "invoice-app-90dbd",
+    storageBucket: "invoice-app-90dbd.appspot.com",
+    messagingSenderId: "41498411767",
+    appId: "1:41498411767:web:71595e02971267a84949f2"
+});
+
+const db = getFirestore(firebaseApp);
+
+export default { db, doc, setDoc }
